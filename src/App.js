b2d3b89ac6251c53486379ac48f42eb2f52ad76e
florvillafane <florencia.a.villafane@gmail.com>
2022-01-19 22:06:48 -0300

    segundo commit con gitignore

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Salon de Belleza</h1>
+        <p>
+        ARC - Nails
+        </p>
+        <p>Ejercicio</p>
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
